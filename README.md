7b1999aa20a033160451a5905d132f142b6518c1
Yurino Oga <yurino.o@latona.io>
2021-10-30 12:10:33 +0900

    release

---
+++ README.md
@@ -0,0 +1,8 @@
+# sap-bom-sql 
+sap-bom-sql は、主にエッジアプリケーションにおいて、SAPと連携されたBOM（部品表）データを保存するSQLテーブルを作成するためのレポジトリです。  
+
+## sqlの設定ファイル
+sap-bom-sql には、sqlの設定ファイルとして、sap-bom-sql.sqlが含まれています。
+
+## MySQLのセットアップ / Kubernetesの設定 / SQLテーブルの作成方法
+MySQLのセットアップ / Kubernetesの設定 / 具体的なSQLテーブルの作成方法、については、mysql-kubeを参照ください。
---
+++ sap-bom-sql.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `sap-bom`
+(
+    `BillOfMaterial`        varchar(8) DEFAULT NULL,
+    `Material`              varchar(40) DEFAULT NULL,
+    `Plant`                 varchar(4) DEFAULT NULL,
+    `BillOfMaterialVariant` varchar(2) DEFAULT NULL,
+    `ValidityStartDate`     date DEFAULT NULL,
+    `ValidityEndDate`       date DEFAULT NULL,
+    `HeaderIsDeleted`       varchar(1) DEFAULT NULL,
+    `BillOfMaterialItemNodeNumber` int(8) DEFAULT NULL,
+    `BillOfMaterialComponent` varchar(40) DEFAULT NULL,
+    `BillOfMaterialItemQuantity` float(13) DEFAULT NULL,
+    `ComponentScrapInPercent` float(5) DEFAULT NULL,
+    `ItemIsDeleted`         varchar(1) DEFAULT NULL,
+    PRIMARY KEY (`BillOfMaterial`),
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
