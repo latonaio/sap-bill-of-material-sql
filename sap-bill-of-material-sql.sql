02fe9ff9c1ff2dd0af2b538f159e1e6c4ecf2753
Yurino Oga <yurino.o@latona.io>
2021-11-28 13:38:22 +0900

    release

--- sap-bill-of-material-sql.sql
+++ sap-bill-of-material-sql.sql
@@ -6,12 +6,12 @@ CREATE TABLE `sap-bill-of-material`
     `BillOfMaterialVariant` varchar(2) DEFAULT NULL,
     `ValidityStartDate`     date DEFAULT NULL,
     `ValidityEndDate`       date DEFAULT NULL,
-    `HeaderIsDeleted`       varchar(1) DEFAULT NULL,
-    `BillOfMaterialItemNodeNumber` int(8) DEFAULT NULL,
+    `HeaderIsDeleted`       tinyint(1) DEFAULT NULL,
+    `BillOfMaterialItemNodeNumber` varchar(8) DEFAULT NULL,
     `BillOfMaterialComponent` varchar(40) DEFAULT NULL,
-    `BillOfMaterialItemQuantity` float(13) DEFAULT NULL,
-    `ComponentScrapInPercent` float(5) DEFAULT NULL,
-    `ItemIsDeleted`         varchar(1) DEFAULT NULL,
+    `BillOfMaterialItemQuantity` varchar(13) DEFAULT NULL,
+    `ComponentScrapInPercent` varchar(5) DEFAULT NULL,
+    `ItemIsDeleted`         tinyint(1) DEFAULT NULL,
     PRIMARY KEY (`BillOfMaterial`, `BillOfMaterialItemNodeNumber`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
